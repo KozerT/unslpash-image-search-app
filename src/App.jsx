0f10689f1a12ff8c25f4788bf4aa030e68da8145
KozerT <kozer.vizastaff@gmail.com>
2024-04-10 22:19:46 +0200

    feat: add and style  a configurable Accordion and AccordionItem components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-import "./App.css";
+import Accordion from "./components/accordion/Accordion";
+import AccordionItem from "./components/accordion/AccordionItem";
 
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            title="We have a 20 years of experience"
+            className="accordion-item"
+          >
+            <article>
+              <p>You can&apos;t go wrong with us. </p>
+              <p>We are in the business of planning trips for 20 years</p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            title="Plan Your Trip with Confidence"
+            className="accordion-item"
+          >
+            <article>
+              <p> Let us take the stress out of travel planning. </p>
+              <p>
+                With our proven track record, you can relax knowing your trip is
+                in good hands.
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            title="Unforgettable Trips, Guaranteed"
+            className="accordion-item"
+          >
+            <article>
+              <p>
+                We&apos;re so confident you&apos;ll love your trip that we
+                guarantee your satisfaction.
+              </p>
+              <p>Let us plan your next adventure, worry-free.</p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
