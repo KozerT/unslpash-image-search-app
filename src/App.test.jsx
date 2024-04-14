5db48586e8a9318f2b1e598d8aaf7a198949977a
KozerT <kozer.vizastaff@gmail.com>
2024-04-14 21:55:00 +0200

    chore: add test cases to test App component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,14 +3,54 @@ import App from "./App";
 import { describe } from "vitest";
 
 describe("App component", () => {
-  test("renders main section with Accordion and SearchableList", () => {
-    // Arrange: Render the App component
+  test("renders main section", () => {
     render(<App />);
-
-    // Act: Nothing to do here since we are just rendering the component
-
-    // Assert: Check if the main section, Accordion, and SearchableList are rendered
     const mainSection = screen.getByRole("main");
     expect(mainSection).toBeInTheDocument();
+  });
+
+  test("renders h2 heading", () => {
+    const { getByRole } = render(<App />);
+    const headingElement = getByRole("heading", {
+      name: /Main Section/i,
+      level: 2,
+    });
+    expect(headingElement).toBeInTheDocument;
+  });
+
+  test("renders Accordion component with the class 'accordion' ", () => {
+    const { container } = render(<App />);
+    const accordionComponent = container.querySelector(".accordion");
+    expect(accordionComponent).toBeInTheDocument();
+  });
+
+  test("renders multiple AccordionItems with the class 'accordion-item' ", () => {
+    const { container } = render(<App />);
+    const accordionItems = container.querySelectorAll(".accordion-item");
+
+    expect(accordionItems.length).toBeGreaterThan(1);
+
+    accordionItems.forEach((item) => {
+      expect(item).toBeInTheDocument();
+    });
+  });
+
+  test("renders multiple Accordion Item Titles with the class 'accordion-item-title' ", () => {
+    const { container } = render(<App />);
+    const accordionItemTitles = container.querySelectorAll(
+      ".accordion-item-title"
+    );
+
+    accordionItemTitles.forEach((title) => {
+      expect(title).toBeInTheDocument();
+    });
+  });
+
+  test("renders multiple 'Accordion Content' component", () => {
+    const { container } = render(<App />);
+    const articleElements = container.querySelectorAll("accordion-content");
+    articleElements.forEach((article) => {
+      expect(article).toBeInTheDocument();
+    });
   });
 });
